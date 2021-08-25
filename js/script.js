fae3c717d672915019a1cf7e8304a068cbb3a657
manzaero <ligoero@gmail.com>
2021-08-25 16:38:33 +0500

    new

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ console.log(typeof getNan);*/
 const bigNam = 1n;
 console.log(typeof bigNam);*/
 
-let userName = "overseer";
+/*let userName = "overseer";
 console.log(typeof userName)
 
 let userName2 = 'Overseer';
@@ -38,5 +38,55 @@ console.log(typeof userName3);
 
 let myAge = 25;
 let usserName4Age = `Overseer возраст: ${myAge}`;
-console.log(usserName4Age);
+console.log(usserName4Age);*/
+/*
+let userInfo = {
+	name: 'Overseer',
+	age: 32,
+}
+console.log(userInfo);
+console.log(typeof userInfo)*/
+/*
+let id = Symbol('id');
+console.log(typeof id)*/
+/*
+let funcVariable = function name(params) {
+	//код функций
+}
+console.log(typeof funcVariable);*/
+/*
+let userAge = 32;
+console.log(userAge);
+console.log(typeof userAge);
 
+userAge = String(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);*/
+/*
+let userTrue = true;
+console.log(userTrue);
+console.log(typeof userTrue);
+
+userTrue = String(userTrue);
+
+console.log(userTrue);
+console.log(typeof userTrue);*/
+/*
+let userAge = "40";
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Number(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);*/
+/*
+let userAge = "Если число изменить на текст, тогда выдаст ошибку NaN";
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Number(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);*/
