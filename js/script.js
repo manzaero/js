f32627f49dfa97c44ebe7c263089cbc557b6c6ba
manzaero <ligoero@gmail.com>
2021-08-25 15:57:44 +0500

    туц

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/*let mySmile = true;
+if (mySmile) {
+	console.log(':-)');
+} else {
+	console.log(':-(');
+}*/
+
+/*let trueOrFalse = 58 > 20;
+console.log(trueOrFalse);*/
+/*
+let userAge = 20;
+let userHeight = 1.38;
+
+console.log(userAge);
+console.log(typeof userAge);
+console.log(userHeight);
+console.log(typeof userHeight)*/
+/*
+let myJob = -50 / 0;
+console.log(myJob);
+console.log(typeof myJob);*/
+
+/*let getNan = 'overseer' / 20;
+console.log(getNan);
+console.log(typeof getNan);*/
+/*
+const bigNam = 1n;
+console.log(typeof bigNam);*/
+
+let userName = "overseer";
+console.log(typeof userName)
+
+let userName2 = 'Overseer';
+console.log(typeof userName2);
+
+let userName3 = `Overseer`;
+console.log(typeof userName3);
+
+let myAge = 25;
+let usserName4Age = `Overseer возраст: ${myAge}`;
+console.log(usserName4Age);
+
